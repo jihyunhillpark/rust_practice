91ebaa8744a22678f70ba878565ba2b2b49a81ae
jihyunhillpark <jihyunhillpark@github.com>
2020-02-06 05:05:04 +0000

    Done cmd ls & get <file> from client to server

--- TCPpractice/client.rs
+++ TCPpractice/client.rs
@@ -1,32 +1,109 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
+use std::fs::File;
+use std::fs;
+use std::path::Path;
+//use std::env;
 
+fn printmenu(){
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    println!("ls : lists current directory");
+    println!("get <file name> : get a file from server");
+    println!("q : quit the program");
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+}
+fn second_arg(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b'\n' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
 fn main() {
+    /* 인자 받기 */
+    //let args: Vec<String> = env::args().collect();
+    //let option = String::from(&args[1]);
+    //println!("{:?}", args);
+
     match TcpStream::connect("192.168.0.214:9999") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 9999");
+            printmenu();
 
-            let msg = b"ls";
-            stream.write(msg).unwrap();
+            let mut option = String::new();
 
-            let msg2 = from_utf8(msg).unwrap();
-            println!("Sent {}", msg2);
+            std::io::stdin().read_line(&mut option).expect("Failed to read line");
 
-            let mut data = [0 as u8; 2]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                        let text = from_utf8(&data).unwrap();
-                        println!("Reply from SERVER : {}", text);
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
+            if option.len() > 2 {
+                match &option[0..3] {
+                    "ls\n" =>
+                    {
+                        let msg = b"ls";
+                        stream.write(msg).unwrap();
+
+                        //let msg2 = from_utf8(msg).unwrap();
+                        //println!("Sent {}", msg2);
+
+                        let mut data = [0 as u8; 512]; // using 6 byte buffer
+                        match stream.read(&mut data) {
+                            Ok(_) => {
+                                let text = from_utf8(&data).unwrap();
+                                println!("{}",text);
+                            },
+                            Err(e) => {
+                                println!("Failed to receive data: {}", e);
+                            }
+                        }
                     }
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
+                    "get" =>
+                    {
+                        /*TO DO*/
+                        stream.write(option.as_bytes()).unwrap();
+                        let mut data2 = [0 as u8; 10000];
+                        let second_argument = &option[4..].trim();
+                        let filename = second_arg(&second_argument);
+
+                        match stream.read(&mut data2) {
+                            Ok(_) => {
+                                if &data2[0..5] == b"error" {
+                                    println!("Failed to get {} - Not existing", filename);
+                                }
+                                else{
+                                    match fs::create_dir("out") {
+                                        Err(why) => println!("Out put direcotry {:?}. The file will be located in ./out", why.kind()),
+                                        Ok(_) => {},
+                                    }
+                                    let path = Path::new("out/");
+                                    let temp_file = path.join(filename);
+
+                                    let mut file = File::create(temp_file).unwrap();
+                                    match file.write_all(&data2[..]) {
+                                        Err(why) => println!("! {:?}", why.kind()),
+                                        Ok(_) => {},
+                                    }
+                                    //let text = from_utf8(&data2).unwrap();
+                                    //println!("{}",text);
+                                }
+                            },
+                            Err(e) => {
+                                println!("Failed to receive data: {}", e);
+                            }
+                        }
+
+                    }
+                    _=> println!("wrong input. Try again."),
+                }
+            }
+            else{
+                if option.trim() == String::from("q"){
+                    println!("Quit the program");
+                }
+                else{
+                    println!("wrong input. Try again.")
                 }
             }
         },
--- TCPpractice/server.rs
+++ TCPpractice/server.rs
@@ -1,27 +1,79 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
+use std::fs::File;
 use std::io::{Read, Write};
 use std::str::from_utf8;
+use std::process::Command;
 
 fn handle_client(mut stream: TcpStream) {
     let mut data = [0 as u8; 50]; // using 50 byte buffer
-    while match stream.read(&mut data) {
-        Ok(size) => {
-            // echo everything!
-            let text = from_utf8(&data).unwrap(); //println!을 위해서 바꿔줄 것.
-            println!("client says {}", text);
-            stream.write(&data[0..size]).unwrap(); //data[]안에 0..size안쓰면 thread error뜬다.
-            //Q?여기서 write의 인자가 slice여야 하기 때문 data의 일부분 즉 slice를 쓰는 것인가.
-            true
+    let _first_ls = "l".to_string();
+    let _first_get = "g".to_string();
+
+    match stream.read(&mut data) {
+        Ok(_size) => {
+            // echo
+            let clientc = from_utf8(&data).unwrap();
+            let cmp = String::from(clientc[0..1].to_string());
+
+            match &cmp[..] {
+                "l" => {
+                    println!("...Listing current directory in Client...");
+                    let output = Command::new("ls").output()
+                                                   .expect("ls command failed to start");
+                    stream.write(&output.stdout).unwrap();
+                },
+                "g" => {
+                    let second_argument = &clientc[4..].trim();
+                    let filename = second_arg(&second_argument);
+                    println!("...Find {} asap!!...", filename);
+
+                    let filecontents = read_a_file(&filename);
+                    if  &filecontents[..] == "error" {
+                        stream.write(b"error").unwrap();
+                    }
+                    else{
+                        stream.write(&filecontents.as_bytes()).unwrap();
+                    }
+                },
+                _ => {},
+            }
         },
         Err(_) => {
             println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
             stream.shutdown(Shutdown::Both).unwrap();
-            false
+            //false
         }
-    } {}
+    }
 }
+fn read_a_file(filename : &str) -> String {
+    match File::open(filename.trim()){
+        Ok(mut file) => {
+            let mut content = String::new();
 
+            // Read all the file content into a variable (ignoring the result of the operation).
+            file.read_to_string(&mut content).unwrap();
+            content
+            // The file is automatically closed when is goes out of scope.
+        },
+        // Error handling.
+        Err(_error) => {
+            println!("There is no such file");
+            let error_msg = String::from("error");
+            error_msg
+        },
+    }
+}
+fn second_arg(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b'\n' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:9999").unwrap();
     // accept connections and process them, spawning a new thread for each one
